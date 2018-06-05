8e6cb3e9cb661f2d9f9b005609252f17ab32edfc
Monkey <monkey@gmail.com>
2018-06-05 18:45:56 +0800

    first commit

--- src/com/monkey/Array.java
+++ src/com/monkey/Array.java
@@ -34,6 +34,12 @@ public class Array {
         add(size, value);
     }
 
+    /**
+     * 插入元素
+     *
+     * @param index 指定位置
+     * @param value
+     */
     public void add(int index, int value) {
         if (size == data.length) {
             throw new IllegalArgumentException("Add failed,array is full.");
@@ -42,10 +48,80 @@ public class Array {
             throw new IllegalArgumentException("Add failed,require index >=0 and index <= size.");
         }
         for (int i = size - 1; i >= index; i--) {
-            data[i + 1] = data[i];
+            data[i + 1] = data[i];//index之后的元素向后移一位
         }
         data[index] = value;
         size++;
+    }
+
+    /**
+     * 删除元素，并返回删除的元素
+     *
+     * @param index 指定位置
+     * @return
+     */
+    public int remove(int index) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("Remove failed,require index >=0 and index <= size.");
+        }
+        int value = data[index];
+        for (int i = index; i < size; i++) {
+            data[i] = data[i + 1];
+        }
+        size--;
+        return value;
+    }
+
+    public int removeFirst() {
+        return remove(0);
+    }
+
+    public int removeLast() {
+        return remove(size - 1);
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Get failed,index is illegal.");
+        }
+        return data[index];
+    }
+
+    public void set(int index, int value) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Get failed,index is illegal.");
+        }
+        data[index] = value;
+    }
+
+    /**
+     * 查找数组中是否有元素value
+     *
+     * @param value
+     * @return
+     */
+    public boolean contains(int value) {
+        for (int i = 0; i < size; i++) {
+            if (data[i] == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 查找数组中元素value所在的索引，不存在则返回-1
+     *
+     * @param value
+     * @return
+     */
+    public int find(int value) {
+        for (int i = 0; i < size; i++) {
+            if (data[i] == value) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
--- src/com/monkey/Main.java
+++ src/com/monkey/Main.java
@@ -7,6 +7,8 @@ public class Main {
         for (int i = 0; i < 10; i++) {
             arr.addLast(i);
         }
+        arr.add(1, 100);
+        arr.addFirst(-1);
         System.out.println(arr);
     }
 }
