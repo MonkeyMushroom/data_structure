87550703a852992743f9bdbb6805ee080ec41051
Mr.Monkey <1334352184@qq.com>
2018-06-07 23:42:27 +0800

    array queue

---
+++ src/com/monkey/queue/Queue.java
@@ -0,0 +1,32 @@
+package com.monkey.queue;
+
+/**
+ * First In First Out
+ */
+public interface Queue<E> {
+
+    /**
+     * 获取实际大小
+     */
+    int getSize();
+
+    /**
+     * 是否为空
+     */
+    boolean isEmpty();
+
+    /**
+     * 入队
+     */
+    void enqueue(E e);
+
+    /**
+     * 出队
+     */
+    E dequeue();
+
+    /**
+     * 瞧一眼队首元素
+     */
+    E getFront();
+}
