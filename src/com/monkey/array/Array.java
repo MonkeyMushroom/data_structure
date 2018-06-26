7b0da0fdcc1c1a145a060a78e6824909a9eb9737
Monkey <monkey@gmail.com>
2018-06-26 13:43:19 +0800

    heap

--- src/com/monkey/array/Array.java
+++ src/com/monkey/array/Array.java
@@ -17,6 +17,14 @@ public class Array<E> {
         this(10);
     }
 
+    public Array(E[] arr) {
+        data = (E[]) new Object[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            data[i] = arr[i];
+        }
+        size = arr.length;
+    }
+
     public int getSize() {
         return size;
     }
@@ -143,6 +151,19 @@ public class Array<E> {
         }
         return -1;
     }
+
+    /**
+     * 交换位置
+     */
+    public void swap(int i, int j) {
+        if (i < 0 || i >= size || j < 0 || j >= size) {
+            throw new IllegalArgumentException("out of index");
+        }
+        E temp = data[i];
+        data[i] = data[j];
+        data[j] = temp;
+    }
+
 
     @Override
     public String toString() {
